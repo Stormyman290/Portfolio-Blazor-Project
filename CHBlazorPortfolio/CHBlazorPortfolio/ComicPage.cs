f73bdedac1947006e565c82806239be4329cc5ca
Christopher Hahn <comet.chris@sbcglobal.net>
2023-03-13 22:20:39 -0500

    3/13 Main Tab Progress

---
+++ CHBlazorPortfolio/CHBlazorPortfolio/ComicPage.cs
@@ -0,0 +1,12 @@
+﻿namespace CHBlazorPortfolio
+{
+    public class ComicPage
+    {
+        public string fileName;
+        public string? comment { get; set; }
+
+        public ComicPage(string fileName) : this(fileName, comment: "") { }
+
+        public ComicPage(string fileName, string comment) { this.fileName = fileName; this.comment = comment; }
+    }
+}
