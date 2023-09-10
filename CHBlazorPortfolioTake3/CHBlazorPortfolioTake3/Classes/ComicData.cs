efe7a4efc6531e081d5a30e6ac7da5c933543d45
Christopher Hahn <comet.chris@sbcglobal.net>
2023-09-10 17:16:07 -0500

    9/10 Take3 + Sammy Help
     - Got the basic comic page list that can switch between comic pages
     - Have a few TODOs (in code)
    
    Co-Authored-By: Sammy Mahmoudi <sammymahmoudi@gmail.com>

---
+++ CHBlazorPortfolioTake3/CHBlazorPortfolioTake3/Classes/ComicData.cs
@@ -0,0 +1,24 @@
+﻿namespace CHBlazorPortfolioTake3.Classes
+{
+    public class ComicData
+    {
+        public string comicTitle = "Default";
+        public string[] comicURL = new string[] { "ComicPages/QuestForJonuts-OriginalRun-01.png" };
+        public string commentParagraph = "Default 2oo";
+
+        //TODO Should I have a connection the next and previouis pages???
+
+
+        public ComicData()
+        {
+
+        }
+
+        public ComicData(string comicTitle, string[] comicURL, string commentParagraph)
+        {
+            this.comicTitle = comicTitle;
+            this.comicURL = comicURL;
+            this.commentParagraph = commentParagraph;
+        }
+    }
+}
---
+++ CHBlazorPortfolioTake3/CHBlazorPortfolioTake3/Program.cs
@@ -0,0 +1,21 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using CHBlazorPortfolioTake3;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
+
+await builder.Build().RunAsync();
